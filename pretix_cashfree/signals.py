f77168ebe926bfbbfe398a1d437910bc5e9f0e09
Diptangshu Chakrabarty <cdiptangshu@gmail.com>
2025-08-20 23:42:04 +0530

    fix: formatting issue

--- pretix_cashfree/signals.py
+++ pretix_cashfree/signals.py
@@ -2,8 +2,6 @@
 from django.dispatch import receiver
 from pretix.base.signals import register_payment_providers
 
-from .payment import Cashfree
-
 
 @receiver(register_payment_providers, dispatch_uid="payment_cashfree")
 def register_payment_provider(sender, **kwargs):
