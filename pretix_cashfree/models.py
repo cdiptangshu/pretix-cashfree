2a5069c82c6e412e6a554cd81b590ac7f5040ddf
Diptangshu Chakrabarty <cdiptangshu@gmail.com>
2025-09-06 10:30:32 +0000

    task: log webhooks and handle idempotency. Closes #13, #7, #3

--- pretix_cashfree/models.py
+++ pretix_cashfree/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
 
-class ReferencedCashfreeObject(models.Model):
-    reference = models.CharField(max_length=190, db_index=True, unique=True)
+class PaymentAttempt(models.Model):
+    order_id = models.CharField(max_length=190, db_index=True, unique=True)
     payment = models.ForeignKey(
-        "pretixbase.OrderPayment", null=True, blank=True, on_delete=models.CASCADE
+        "pretixbase.OrderPayment",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        help_text="Latest payment attempt for this order",
     )
+
+
+class PaymentWebhookEvent(models.Model):
+    cf_payment_id = models.CharField(max_length=50, db_index=True, unique=True)
+    order_code = models.CharField(max_length=50)
+    payment_local_id = models.CharField(max_length=50)
+    payment_status = models.CharField(max_length=50)
+    payload = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
