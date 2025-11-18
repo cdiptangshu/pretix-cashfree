c7acaf192ac6ebd24068135024c302e98dd1e914
Diptangshu Chakrabarty <cdiptangshu@gmail.com>
2025-11-18 19:14:42 +0000

    feat: add cashfree client id and client secret in global settings.

--- pretix_cashfree/signals.py
+++ pretix_cashfree/signals.py
@@ -1,6 +1,14 @@
 # Register your receivers here
+from collections import OrderedDict
 from django.dispatch import receiver
-from pretix.base.signals import register_payment_providers
+from pretix.base.signals import register_payment_providers, register_global_settings
+from collections import OrderedDict
+
+from django import forms
+from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
+
+from pretix.base.forms import SecretKeySettingsField
 
 
 @receiver(register_payment_providers, dispatch_uid="payment_cashfree")
@@ -8,3 +16,16 @@ def register_payment_provider(sender, **kwargs):
     from .payment import CashfreePaymentProvider
 
     return CashfreePaymentProvider
+
+@receiver(register_global_settings, dispatch_uid='cashfree_global_settings')
+def register_global_settings(sender, **kwargs):
+    return OrderedDict([
+        ('payment_cashfree_global_client_id', forms.CharField(
+            label=_('Cashfree Client ID'),
+            required=False,
+        )),
+        ('payment_cashfree_global_client_secret', SecretKeySettingsField(
+            label=_('Cashfree Client Secret'),
+            required=False,
+        )),
+    ])
